7e2ead4a32b10574060584b266025ad2bca0981b
Predrag Milanovic <predrag.milanovic@apeiron-edu.eu>
2023-01-15 19:40:22 +0100

    05-04

--- src/server.js
+++ src/server.js
@@ -2,14 +2,20 @@ var express = require('express')
 var  app = express()
 
 app.use(express.static(__dirname))
+
 var messages = [
     {name: 'Tim', messages: 'Hi'},
     {name: 'Jane', messages: 'Hello'}
 ]
 
 app.get('/messages', (req, res)=>{
-    res.send('hello')
+    res.send(messages)
 })
+
+app.post('/messages', (req, res)=>{
+    res.sendStatus(200)
+})
+
 var server = app.listen(3000, () => {
     console.log('server is listening on port', server.address().port)
 })
