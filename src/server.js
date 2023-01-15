a476eb7e480e856de47b6dc372e8e86ad02eec49
Predrag Milanovic <predrag.milanovic@apeiron-edu.eu>
2023-01-15 20:14:35 +0100

    05.04

--- src/server.js
+++ src/server.js
@@ -4,17 +4,18 @@ var  app = express()
 
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
 
 var messages = [
     {name: 'Tim', messages: 'Hi'},
     {name: 'Jane', messages: 'Hello'}
 ]
 
-app.get('/messages', (req, res)=>{
+app.get('/messages', (req, res) => {
     res.send(messages)
 })
 
-app.post('/messages', (req, res)=>{
+app.post('/messages', (req, res) => {
     messages.push(req.body)
     res.sendStatus(200)
 })
