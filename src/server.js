a70d7665a9895875de1a8cec3dfba7efb0e6a2e4
Predrag Milanovic <predrag.milanovic@apeiron-edu.eu>
2023-01-18 00:31:01 +0100

    update 18.01.

--- src/server.js
+++ src/server.js
@@ -9,7 +9,9 @@ app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
-var dbUrl = 'mongodb+srv://webdevpr:<225883>@cluster0.1agwbe6.mongodb.net/test'
+mongoose.Promise = Promise
+
+var dbUrl = 'mongodb+srv://webdevpr:password>@cluster0.1agwbe6.mongodb.net/test'
 //need to make a configuration file for security
 
 var Message = mongoose.model('Message', {
@@ -26,23 +28,31 @@ app.get('/messages', (req, res) => {
 app.post('/messages', (req, res) => {
     var message = new Message(req.body)
 
-    message.save((err) => {
-        if(err)
-        sendStatus(500)
-
+    message.save().then(() => {
+        console.log('saved')
+        return Message.findOne({message:'badword'})
+    })
+    .then(censored = {
+        if(censored) {
+            console.log('censored words found', censored)
+            Message.remove({_id: censored.id}, (err) => {
+                console.log('removed censored message')
+            })
+        }
         io.emit('message', req.body)
         res.sendStatus(200)
     })
-})
+    .catch((err) => {
+        res.sendStatus(500)
+        return console.error(err)
+    })
+}
 
-    
-
-io.on('connection', (Socket) => {
+io.on('connection', (socket) => {
     console.log('a user connected')
 })
 
-
-mongoose.connect(dbUrl, {useMongoClient: true}, (err) = {
+mongoose.connect(dbUrl, {useMongoClient: true}, (err) => {
     console.log('mongo db connection', err)
 })
 
