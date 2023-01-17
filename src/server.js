4e1f1353b1049429864bf697e6c87b354f80c8f6
Predrag Milanovic <predrag.milanovic@apeiron-edu.eu>
2023-01-17 13:25:35 +0100

    add mongoDB and mongoose

--- src/server.js
+++ src/server.js
@@ -1,13 +1,16 @@
 var express = require('express')
 var bodyParser = require('body-parser')
-const { Socket } = require('dgram')
 var app = express()
 var http = require('http').server(app)
 var io = require('socket.io')(http)
+var mongoose = require('mongoose')
 
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
+
+var dbUrl = 'mongodb+srv://webdevpr:<225883>@cluster0.1agwbe6.mongodb.net/test'
+//need to make a configuration file for security
 
 var messages = [
     {name: 'Tim', messages: 'Hi'},
@@ -26,6 +29,11 @@ app.post('/messages', (req, res) => {
 
 io.on('connection', (Socket) => {
     console.log('a user connected')
+})
+
+
+mongoose.connect(dbUrl, {useMongoClient: true}, (err) = {
+    console.log('mongo db connection', err)
 })
 
 var server = http.listen(3000, () => {
